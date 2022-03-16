22042083b21e4f9ddfe878abc0f32504b5217201
Pierr Grimaldo <pierr_grimaldo@hotmail.com>
2022-03-16 10:30:18 -0500

    .

--- GrupoCiencias.Intranet.Application.Interfaces/IMatriculaRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace GrupoCiencias.Intranet.Application.Interfaces
-{
-    public interface IMatriculaRepository
-    {
-        Task<int> ObtenerIdApoderadoAsync(string DNIApoderado);
-    }
-}
