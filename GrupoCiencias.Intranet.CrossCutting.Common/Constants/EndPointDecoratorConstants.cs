e0304001fe1a37a455ddd82d41b64142ef3003fc
Pierr Grimaldo <pierr_grimaldo@hotmail.com>
2022-03-17 00:34:44 -0500

    .
    .

--- GrupoCiencias.Intranet.CrossCutting.Common/Constants/EndPointDecoratorConstants.cs
+++ GrupoCiencias.Intranet.CrossCutting.Common/Constants/EndPointDecoratorConstants.cs
@@ -2,15 +2,21 @@
 {
     public  class EndPointDecoratorConstants
     {
-        public struct MasterRouter
+        public struct EndPointRouter
         {
             public const string GetListMasterDetail = "GetListMasterDetail"; 
+            public const string Authentication = "Authentication";
         }
 
         public struct MatriculaRouter
         {
             public const string RegistrarSolicitud = "RegisterEnrollment";
             public const string GetListMatriculaPrices = "GetEnrollmentPricesList";
+        }
+        
+        public struct MercadoPagoEndPointRouter
+        {
+            public const string CardToken = "CardToken";
         }
     }
 }
