b209533d4350b4a48109fb12067e960113d539b0
Pierr Grimaldo <pierr_grimaldo@hotmail.com>
2022-06-07 01:53:46 -0500

    Pase a producción
    Pase a producción

--- GrupoCiencias.Intranet.Domain.Models/Entity/TipoPagoDetalleEntity.cs
+++ GrupoCiencias.Intranet.Domain.Models/Entity/TipoPagoDetalleEntity.cs
@@ -1,4 +1,6 @@
-﻿namespace GrupoCiencias.Intranet.Domain.Models.Entity
+﻿using System;
+
+namespace GrupoCiencias.Intranet.Domain.Models.Entity
 {
     public partial class TipoPagoDetalleEntity
     {
@@ -9,5 +11,9 @@
         public decimal? Descuento { get; set; }
         public decimal Preciofinal { get; set; }
         public int? IdSede { get; set; }
+        public int Activo { get; set; }
+        public string Usuario { get; set; } 
+        public DateTime? Fechabaja { get; set; }
+        public DateTime Fechacreacion { get; set; }
     }
 }
--- GrupoCiencias.Intranet.Repository.Implementations/Configuration/TipoPagoDetalleConfiguration.cs
+++ GrupoCiencias.Intranet.Repository.Implementations/Configuration/TipoPagoDetalleConfiguration.cs
@@ -9,17 +9,27 @@ namespace GrupoCiencias.Intranet.Repository.Implementations.Configuration
         public void Configure(EntityTypeBuilder<TipoPagoDetalleEntity> builder)
         {
             builder.HasKey(e => e.Idpagodetalle)
-                    .HasName("tipo_pago_detalle_pkey");
+                     .HasName("tipo_pago_detalle_pkey");
 
             builder.ToTable("tipo_pago_detalle");
 
             builder.Property(e => e.Idpagodetalle).HasColumnName("idpagodetalle");
 
+            builder.Property(e => e.Activo)
+                .HasColumnName("activo")
+                .HasDefaultValueSql("1");
+
             builder.Property(e => e.Descuento)
                 .HasPrecision(9, 2)
                 .HasColumnName("descuento");
 
+            builder.Property(e => e.Fechabaja)
+                .HasColumnType("date")
+                .HasColumnName("fechabaja");
+
             builder.Property(e => e.Idciclo).HasColumnName("idciclo");
+
+            builder.Property(e => e.IdSede).HasColumnName("idsede");
 
             builder.Property(e => e.Idtipopago).HasColumnName("idtipopago");
 
@@ -31,7 +41,11 @@ namespace GrupoCiencias.Intranet.Repository.Implementations.Configuration
                 .HasPrecision(9, 2)
                 .HasColumnName("subtotal");
 
-            builder.Property(e => e.IdSede).HasColumnName("idsede");
+            builder.Property(e => e.Usuario)
+                .IsRequired()
+                .HasMaxLength(100)
+                .HasColumnName("usuario")
+                .HasDefaultValueSql("'ADMIN'::character varying");
 
         }
     }
